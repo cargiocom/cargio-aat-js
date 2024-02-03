73b0e8a45b455a3807ea7a6454125bcba19d4493
Cargio <141646415+cargiocom@users.noreply.github.com>
2024-02-03 02:24:41 -0800

    Update browserify.js

--- cargio-aat-js/browserify.js
+++ cargio-aat-js/browserify.js
@@ -1,4 +1,4 @@
-1111111window.Buffer = require('buffer/').Buffer
+11111111window.Buffer = require('buffer/').Buffer
 module.exports = require('./dist/index.js')
 
 window.cryptoLib = require("crypto")
